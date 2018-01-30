d0f8702d7318c3da85b44746385ba2f910ba0bd6
Michael Langford <michael.langford@rowdylabs.com>
2018-01-30 12:58:38 -0500

    Framework for showing drugs in

--- pm0/PrescriptionEntryViewController.swift
+++ pm0/PrescriptionEntryViewController.swift
@@ -7,10 +7,74 @@
 //
 
 import UIKit
+import SearchTextField
+
+
+struct DisplayDrug{
+  var name:String
+  var commonUses:[String]
+}
+
+var drugs = [
+  DisplayDrug(name:"Aspirin", commonUses:["Pain Relief", "Inflammation Reduction", "Anti-clotting"]),
+  DisplayDrug(name:"Lipitor", commonUses:["Cholesterol Control"]),
+  DisplayDrug(name:"Advair Diskus", commonUses:["Asthma Attack Prevention"]),
+  DisplayDrug(name:"Metformin", commonUses:["Diabetes Treatment"])
+]
+extension DisplayDrug{
+  var searchTextFieldItem:SearchTextFieldItem{
+    switch commonUses.count{
+    case 0:
+    return SearchTextFieldItem(title:name)
+    case 1:
+    return SearchTextFieldItem(title:name,subtitle:"Common use: \(commonUses.first!)")
+
+    default:
+      let appending:(String,String)->String = {orig, next in return "\(orig), \(next)"}
+      let subtitle = commonUses.reduce("Common uses: ",appending)
+    return SearchTextFieldItem(title:name, subtitle: subtitle)
+    }
+  }
+}
 
 class PrescriptionEntryViewController: UIViewController {
 
-  @IBOutlet weak var medicationNameField:UITextField!
+  @IBOutlet weak var medicationNameField:SearchTextField!
+  @IBOutlet weak var unitQuantityPerDoseField:SearchTextField!
+  @IBOutlet weak var unitDoseField:SearchTextField!
+  @IBOutlet weak var whenIsItTakenField:SearchTextField!
+  @IBOutlet weak var prescribingDoctorField:SearchTextField!
+  @IBOutlet weak var pharmacyField:SearchTextField!
+  @IBOutlet weak var conditionField:SearchTextField!
+
+
+
+  @IBOutlet weak var medicationNameHelpButton: UIButton!
+  @IBOutlet weak var quantityPerDoseHelpButton: UIButton!
+  @IBOutlet weak var unitDoseHelpButton: UIButton!
+  @IBOutlet weak var conditionHelpButton: UIButton!
+  @IBOutlet weak var pharmacyHelpButton: UIButton!
+  @IBOutlet weak var prescribingDoctorHelpButton: UIButton!
+  @IBOutlet weak var whenTakenHelpButton: UIButton!
+
+  @IBAction func doctorContactAddressButtonTapped(_ sender: Any) {
+
+  }
+
+  @IBAction func pharmacyContactAddressButtonTapped(_ sender: Any) {
+  }
+
+
+
+  func drugsForText(str:String?)->[DisplayDrug]{
+    return drugs
+  }
+
+  override func viewDidLoad() {
+    let medicationNameSearchItems = drugsForText(str: medicationNameField?.text).map{$0.searchTextFieldItem}
+    medicationNameField.filterItems(medicationNameSearchItems)
+
+  }
 
   var medicationName:String?{
     return medicationNameField?.text
