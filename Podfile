5a9f13e69e2816d94dcf1a83e1fe3f8be404acb5
Michael Langford <michael.langford@rowdylabs.com>
2018-01-30 12:27:44 -0500

    Generated podfile

---
+++ Podfile
@@ -0,0 +1,9 @@
+
+platform :ios, '11.0'
+use_frameworks!
+
+target "pm0" do
+  pod "SearchTextField"
+end
+
+
---
+++ Pods/Target Support Files/Pods-pm0/Pods-pm0-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_pm0 : NSObject
+@end
+@implementation PodsDummy_Pods_pm0
+@end
