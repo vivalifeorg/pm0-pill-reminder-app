ac6823bacbbc29604c9fe5b4078246225005793e
Michael Langford <michael.langford@rowdylabs.com>
2018-02-13 17:48:53 -0500

    name search function works with DB

--- pm0/FDA_DB/FDADatabase.swift
+++ pm0/FDA_DB/FDADatabase.swift
@@ -10,26 +10,54 @@ import Foundation
 import SQLite
 
 
-struct ProductPackaging{
+struct ProductPackaging:Decodable{
   let productName: String
   let ndcPackageCode: String
   let packageDescription: String
 }
 
+//let drugList = loadDrugDB()
+
+extension Array where Element : Hashable {
+  var unique: [Element] {
+    return Array(Set(self))
+  }
+}
+
+func namesMatching(_ search:String)->[String]{
+  let productName = Expression<String>("productName")
+  print("Searching for '\(search)'")
+  let query = Table("ProductPackage").filter(productName.like("\(search)%"))
+  let queryResults = try? fdaDbConnection.prepare( query )
+  return  queryResults?.map{$0[productName]}.unique ?? []
+}
 
 
-let drugList = loadDrugDB()
-func loadDrugDB()->[ProductPackaging]{
-  let db = try! Connection(Bundle.main.path(forResource: "fda_drugs", ofType: "db")!)
-  let productPackages = Table("ProductPackage")
+func packagesMatching(_ search:String)->[ProductPackaging]{
   let productName = Expression<String>("productName")
   let ndcPackageCode = Expression<String>("ndcPackageCode")
   let packageDescription = Expression<String>("packageDescription")
-  return try! db.prepare(productPackages).map{ packageInfo in
-    ProductPackaging(productName:packageInfo[productName],
-                     ndcPackageCode:packageInfo[ndcPackageCode],
-                     packageDescription:packageInfo[packageDescription])
+  //return try! fdaDbConnection.run(Table("ProductPackage").filter(productName.like("%\(search)%")))
 
+  print("Searching for '\(search)'")
+  let query = Table("ProductPackage").filter(productName.like("%\(search)%"))
+  let queryResults = try? fdaDbConnection.prepare( query )
+  for package in queryResults! {
+    print("Match: \(package[productName]), ndcPackageCode: \(package[ndcPackageCode]),packageDescription: \(package[packageDescription])")
+
+  }
+  return  []
+}
+
+fileprivate let fdaDbConnection = {return try! Connection(Bundle.main.path(forResource: "fda_drugs", ofType: "db")!)}()
+
+fileprivate let productPackageTable = {
+  return try! fdaDbConnection.prepare(Table("ProductPackage"))
+}()
+
+fileprivate func loadDrugDB()->[ProductPackaging]{
+  return productPackageTable.map { row in
+    return try! row.decode()
   }
 }
 
