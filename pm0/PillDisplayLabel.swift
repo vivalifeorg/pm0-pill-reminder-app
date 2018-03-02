04d8d5181a6e7c83f6cf9b6b8f49badd0b483cbe
Michael Langford <michael.langford@rowdylabs.com>
2018-03-02 15:41:57 -0500

    Pill labels extracted out

---
+++ pm0/PillDisplayLabel.swift
@@ -0,0 +1,17 @@
+//
+//  PillDisplayLabel.swift
+//  pm0
+//
+//  Created by Michael Langford on 3/2/18.
+//  Copyright © 2018 Rowdy Labs. All rights reserved.
+//
+
+import UIKit
+
+class PillDisplayLabel: UILabel{
+
+  override func awakeFromNib() {
+    layer.cornerRadius = 4
+    clipsToBounds = true
+  }
+}
