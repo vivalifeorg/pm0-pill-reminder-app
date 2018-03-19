1767901acc9097e167660478d0c00a79eac3e22a
Michael Langford <michael.langford@rowdylabs.com>
2018-03-19 16:35:03 -0400

    Fax test with python lib to debug multipart issues

---
+++ scripts/fax.py
@@ -0,0 +1,23 @@
+
+
+import phaxio
+import os
+
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    # Legacy Python that doesn't verify HTTPS certificates by default
+    pass
+else:
+    # Handle target environment that doesn't support HTTPS verification
+    ssl._create_default_https_context = _create_unverified_https_context
+
+
+api = phaxio.PhaxioApi(os.environ['API_KEY'], os.environ['API_SECRET'])
+response = api.Fax.send(to=['18558237571'],
+    files='../pm0/TestPages/FaxPNG1.png')
+print(response.data.id)
+
+
