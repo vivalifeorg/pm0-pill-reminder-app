74f976f4817a8e314c3743c8585ed0bc7ac7ac86
Michael Langford <michael.langford@rowdylabs.com>
2018-03-22 17:39:39 -0400

    Adds help screen

---
+++ pm0/RxEntry/PrescriptionEntryHelpViewController.swift
@@ -0,0 +1,12 @@
+//
+//  PrescriptionEntryHelpViewController.swift
+//  pm0
+//
+//  Created by Michael Langford on 3/22/18.
+//  Copyright © 2018 Rowdy Labs. All rights reserved.
+//
+import UIKit
+
+class PrescriptionEntryHelpViewController:UIViewController {
+  @IBOutlet weak var helpLabel:UILabel!
+}
