04b6f5696d40ccda8227e8a482843c68e0dd4fe6
Michael Langford <michael.langford@rowdylabs.com>
2018-03-23 15:42:55 -0400

    Pill items

---
+++ pm0/Doctors/DoctorListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DoctorListViewController.swift
+//  pm0
+//
+//  Created by Michael Langford on 3/23/18.
+//  Copyright © 2018 Rowdy Labs. All rights reserved.
+//
+
+import UIKit
+
+class DoctorListViewController:UITableViewController{
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    tableView.tableFooterView = UIView() //remove excess lines
+
+  }
+}
