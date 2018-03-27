8c66e68367598c29417f598ae74d5062d5d7d7b7
Michael Langford <michael.langford@rowdylabs.com>
2018-03-27 10:04:04 -0400

    Fixes storyboard inconsistancy

---
+++ pm0/Doctors/DoctorEntryViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DoctorEntryViewController.swift
+//  pm0
+//
+//  Created by Michael Langford on 3/27/18.
+//  Copyright © 2018 Rowdy Labs. All rights reserved.
+//
+
+import UIKit
+
+class DoctorEntryViewController:UIViewController{
+
+}
