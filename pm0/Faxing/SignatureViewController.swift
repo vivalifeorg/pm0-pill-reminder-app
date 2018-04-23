162c2863a0865c093a5378de546f0f176b4e34ee
Michael Langford <michael.langford@rowdylabs.com>
2018-04-23 18:08:04 -0400

    reconnects send fax button

--- pm0/Faxing/SignatureViewController.swift
+++ pm0/Faxing/SignatureViewController.swift
@@ -36,6 +36,19 @@ extension UIImage {
 
     return newImage
   }
+
+    func invertedImage() -> UIImage? {
+      guard let cgImage = self.cgImage else { return nil }
+      let ciImage = CoreImage.CIImage(cgImage: cgImage)
+      guard let filter = CIFilter(name: "CIColorInvert") else { return nil }
+      filter.setDefaults()
+      filter.setValue(ciImage, forKey: kCIInputImageKey)
+      let context = CIContext(options: nil)
+      guard let outputImage = filter.outputImage else { return nil }
+      guard let outputImageCopy = context.createCGImage(outputImage, from: outputImage.extent) else { return nil }
+      return UIImage(cgImage: outputImageCopy)
+    }
+
 }
 
 class SignatureViewController:UIViewController, PDFHandler, SendableDocumentMetadata,RestrictionsMetadata, SignatureViewDelegate{
@@ -58,10 +71,11 @@ class SignatureViewController:UIViewController, PDFHandler, SendableDocumentMeta
   }
 
   func SignatureViewDidCaptureSignature(view: SignatureView, signature: Signature?){
-    guard let signature = signature else{
+    guard let signature = signature,
+      let correctedImage = signature.image.convertedToGrayImage()?.invertedImage() else{
       return
     }
-    latestSignature = SignatureInfo(image:signature.image, date:signature.date)
+    latestSignature = SignatureInfo(image:correctedImage, date:signature.date)
   }
   
   var latestSignature:SignatureInfo?
