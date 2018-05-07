213ea62f29fffc4c18e1657af9dca88d53df96d1
Michael Langford <michael.langford@rowdylabs.com>
2018-05-07 17:57:08 -0400

    removed the files from the cocoapods project that get re-added by down

--- pm0/General/AppDelegate.swift
+++ pm0/General/AppDelegate.swift
@@ -26,6 +26,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func applicationDidEnterBackground(_ application: UIApplication) {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+
+    ///this is what makes the app logout when the lock screen comes on or they swap to another app
     NotificationCenter.default.post(name: VLNeedsAuthAgainNotification.name, object: nil)
 
   }
