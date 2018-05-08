c031c14ef54068cdbd01ea71dd0203805df42757
Michael Langford <michael.langford@rowdylabs.com>
2018-05-08 16:54:00 -0400

    Fixes first run bug that was hitting them with the lock screen

--- pm0/LockAndOnboarding/LockViewController.swift
+++ pm0/LockAndOnboarding/LockViewController.swift
@@ -20,6 +20,8 @@ class LockViewController:UIViewController{
   override func viewDidAppear(_ animated: Bool) {
     if authInfo.firstRun {
       performSegue(withIdentifier: StoryboardSegue.Main.showOnboarding.rawValue, sender: self)
+    } else if isFirstRunAndJustFinishedOnboarding{
+      performSegue(withIdentifier: StoryboardSegue.Main.goToApp.rawValue, sender: self)
     }
   }
 
@@ -27,6 +29,7 @@ class LockViewController:UIViewController{
     return LocalStorage.AuthenticationStore.load().first ?? AuthenticationInfo()
   }
 
+  private var isFirstRunAndJustFinishedOnboarding = false
   /// record that the user used the app
   func markFirstRun(){
     var authInfo = LocalStorage.AuthenticationStore.load().first ?? AuthenticationInfo()
@@ -54,18 +57,22 @@ class LockViewController:UIViewController{
   /// - Parameter segue: storyboard segue used to get here from onboarding
   @IBAction func unwindHitFinalOnboardingButton(segue:UIStoryboardSegue){
     //On the first run, there is no data to secure, so we can let them into the app
-    guard authInfo.firstRun else {
-      return
-    }
+    autologinIfPossible()
+  }
 
-    markFirstRun()
-    self.performSegue(withIdentifier: StoryboardSegue.Main.goToApp.rawValue, sender: self)
+  func autologinIfPossible(){
+    if authInfo.firstRun {
+      isFirstRunAndJustFinishedOnboarding = true
+      markFirstRun()
+    }
   }
 
   /// User tapped onboarding skip button
   ///
   /// - Parameter segue: segue from storyboard in onboarding
   @IBAction func unwindSkipOnboarding(segue:UIStoryboardSegue){
+    //On the first run, there is no data to secure, so we can let them into the app
+    autologinIfPossible()
   }
 
   /// Used to pop back out to login
